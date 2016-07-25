e96a2cbcb81f8bb799d399b0db937ebf26838a94
lizhongqiangmacpro <328656732@qq.com>
2016-07-25 10:41:06 +0800

    create demo

---
+++ Quartz2DSimpleDemo/Quartz2DSimpleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quartz2DSimpleDemo
+//
+//  Created by LiZhongQiang on 16/7/25.
+//  Copyright © 2016年 LiZhongQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quartz2DSimpleDemo/Quartz2DSimpleDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quartz2DSimpleDemo
+//
+//  Created by LiZhongQiang on 16/7/25.
+//  Copyright © 2016年 LiZhongQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
